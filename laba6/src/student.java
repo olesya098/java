f12fe34daf30aedd1e27b3c7dd00a512976d906d
olesya098 <108861025+olesya098@users.noreply.github.com>
2024-02-28 11:38:29 +0300

    Add files via upload

--- laba6/src/student.java
+++ laba6/src/student.java
@@ -1,36 +1,42 @@
 import java.lang.System;
-import java.util.Arrays;
-import java.util.Comparator;
+
 
 public class student {
         String Name;
         int NumberGroup;
-        double gradies;
+        int [] grades;
 
-        public student(String Name, int NumberGroup, double gradies) {
+        public student(String Name, int NumberGroup, int [] grades) {
             this.Name = Name;
             this.NumberGroup = NumberGroup;
-            this.gradies= gradies;
+            this.grades= grades;
 
         }
+    public double calculate1() {
+        double sum = 0;//сюда суммируется значение всех оценок
+        for (int grade : grades) {
+            sum += grade;//сложение оценок
+        }
+        return (double) sum / grades.length;//деление суммы оценок на длину массива
+    }
         public static void main(String[] args) {
             student[] students = new student[10];
-            students[0] = new  student ("Петров В. В.", 12345,4.45 );
-            students[1] = new student ("Иванов А. А.", 23456,5.0 );
-            students[2] = new student ("Сидоров В. И.", 34567, 3.34);
-            students[3] = new student ("Смирнова О. С.", 45678,5.55 );
-            students[4] = new student ("Козлова Е. В.", 56789, 4.87);
-            students[5] = new  student ("Бублик В. В.", 12345,3.96);
-            students[6] = new student ("Соколова П. А.", 23456,5.00 );
-            students[7] = new student ("Поликов В. И.", 34567, 3.40);
-            students[8] = new student ("Бобров К. К.", 45678,5.00 );
-            students[9] = new student ("Польков Е. В.", 56789, 4.98);
+            students[0] = new  student ("Петров В. В.", 12345,new int[]{3,4,5,2,4} );
+            students[1] = new student ("Иванов А. А.", 23456,new int[]{ 3,5,2,3,4});
+            students[2] = new student ("Сидоров В. И.", 34567, new int[]{3,3,3,3,3});
+            students[3] = new student ("Смирнова О. С.", 45678,new int[]{5,5,5,5,5});
+            students[4] = new student ("Козлова Е. В.", 56789, new int[]{4,4,4,4,4});
+            students[5] = new  student ("Бублик В. В.", 12345,new int[]{2,2,2,2,2});
+            students[6] = new student ("Соколова П. А.", 23456,new int[]{3,4,3,4,3});
+            students[7] = new student ("Поликов В. И.", 34567, new int[]{5,3,5,3,5});
+            students[8] = new student ("Бобров К. К.", 45678,new int[]{4,2,4,2,4 });
+            students[9] = new student ("Польков Е. В.", 56789, new int[]{5,3,4,5,3});
 
             // Сортировка по возрастанию среднего балла методом вставки
             for (int i = 1; i < students.length; i++) {
                 student key = students[i];//сохранение текущего эл-та
                 int j = i - 1;//сравнение с предыдущими эл-тами
-                while (j >= 0 && students[j].gradies > key.gradies) {//сравнение на то что средний балл больше чем у key
+                while (j >= 0 && students[j].calculate1() > key.calculate1()) {//сравнение на то что средний балл больше чем у key
                     //двигаем эл-ты вправо, что б освободить место для начального эл-та
                     students[j + 1] = students[j];//сдвиг
                     j--;//перемещаемся к следующему предыдущему элементу
@@ -40,12 +46,12 @@ public class student {
 
             // Вывод отсортированного массива
             for (student student1 : students) {// используется для прохода по элементам массива
-                System.out.println(student1.Name + ": " + student1.gradies);
+                System.out.println(student1.Name + ": " + student1.calculate1());
             }
             // Вывод отсортированного массива и фамилий студентов с оценками только 4 или 5
             System.out.println("Студенты с оценками только 4 или 5:");
             for (student student : students) {
-                if (student.gradies >= 4 && student.gradies <= 5) {
+                if (student.calculate1() >= 4) {
                     System.out.println("Фамилия: " + student.Name + ", Номер группы: " + student.NumberGroup);
                 }
             }
